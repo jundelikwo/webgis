20783674c7fee185cf41347ddd65fb144e5781c5
Jonathan Utsu Undelikwo <jundelikwo@gmail.com>
2018-10-29 21:54:18 +0100

    Moved home page link to admin&driver nav pages

---
+++ layouts/driver_nav.php
@@ -0,0 +1,1 @@
+<li><a href="<?php echo SITE_LINK ?>">Home</a></li>
--- public/admin/index.php
+++ public/admin/index.php
@@ -24,11 +24,11 @@
 		</div>
 		<div id="page">
 			<h2>Admin Menu</h2>
-			<p>Welcome to the admin area, jonathan.</p>
+			<p>Welcome to the admin area.</p>
 			
 			<ul>
 				<li><a href="manage_content.php">Manage Drivers</a></li>
-				<li><a href="manage_admins.php">Manage Complains</a></li>
+				<li><a href="manage_complain.php">Manage Complains</a></li>
 				<li><a href="manage_admins.php">Manage Bins</a></li>
 				<li><a href="logout.php">Logout</a></li>
 			</ul>
