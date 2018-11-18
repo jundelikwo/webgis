f33cc0b02a6e6ba184af577cadf54fafdbd4f7e5
Jonathan Utsu Undelikwo <jundelikwo@gmail.com>
2018-11-18 20:52:42 +0100

    Display area driver name in manage_area

--- public/admin/manage_area.php
+++ public/admin/manage_area.php
@@ -2,12 +2,18 @@
 	require_once '../../includes/config.php';
 	require_once LIB_PATH.DS.'Session.php';
 	require_once LIB_PATH.DS.'Area.php';
+	require_once LIB_PATH.DS.'User.php';
 
 	if(!$session->isLoggedIn() || !$session->isAdmin()){
 		$link = SITE_LINK.'admin/login.php';
 		header("Location: {$link}");
 	}
 	$message = $session->getFlashValue('message');
+	$allDrivers = User::findAllByField('role','driver');
+	$drivers = [];
+	foreach ($allDrivers as $key => $driver) {
+		$drivers[$driver->id] = $driver->name;
+	}
 	$allAreas = Area::findAll();
 	$layout_context = 'admin';
 	require_once LAYOUT_PATH.DS.'header.php';
@@ -37,6 +43,7 @@
       			<thead>
 				  <tr>
 				    <th style="text-align: left; width: 200px;">Name</th>
+				    <th style="text-align: left; width: 200px;">Driver</th>
 				    <th colspan="2" style="text-align: left;">Actions</th>
 				  </tr>
 				</thead>
@@ -46,6 +53,15 @@
 				    ?>
 			        <tr>
 				    	<td><?php echo substr(htmlentities($area->name),0,10) ?></td>
+				    	<td>
+				    		<?php
+				    			if(array_key_exists($area->driverId, $drivers)){
+				    				echo substr(htmlentities($drivers[$area->driverId]),0,10);
+				    			}else{
+				    				echo "Not yet allocated";
+				    			}
+				    		?>
+				    	</td>
 				        <td><a href="edit_area.php?id=<?php echo urlencode($area->id)?>">Edit</a></td>
 				        <td><a href="delete_area.php?id=<?php echo urlencode($area->id)?>" onclick="return confirm('Are you sure?');">Delete</a></td>
 				    </tr>
